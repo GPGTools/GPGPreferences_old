f117d6da6ecf92bbfc4fad738dc8f9b9ee9a5acb
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2003-02-16 21:03:57 +0000

    Changed license to LGPL (was still GPL!); added pgp7; added recognition of no-pgp2, no-pgp6, no-pgp7

--- GPGCompatibilityPrefs.m
+++ GPGCompatibilityPrefs.m
@@ -5,20 +5,21 @@
 //  Created by davelopper@users.sourceforge.net on Thu Feb 07 2002.
 //
 //
-//  Copyright (C) 2002 Mac GPG Project.
+//  Copyright (C) 2002-2003 Mac GPG Project.
 //  
-//  This code is free software; you can redistribute it and/or modify it under
-//  the terms of the GNU General Public License as published by the Free
-//  Software Foundation; either version 2 of the License, or any later version.
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your options) any later version.
 //  
-//  This code is distributed in the hope that it will be useful, but WITHOUT ANY
-//  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-//  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-//  details.
-//  
-//  For a copy of the GNU General Public License, visit <http://www.gnu.org/> or
-//  write to the Free Software Foundation, Inc., 59 Temple Place--Suite 330,
-//  Boston, MA 02111-1307, USA.
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place--Suite 330, Boston, MA 02111-1307, USA
 //  
 //  More info at <http://macgpg.sourceforge.net/> or <macgpg@rbisland.cx>
 //
@@ -32,6 +33,7 @@ enum {
     rfc1991OptionSet = 0,
     pgp2OptionSet,
     pgp6OptionSet,
+    pgp7OptionSet,
     openPGPOptionSet,
     customOptionSet
 };
@@ -56,7 +58,8 @@ enum {
 {
     NSArray		*optionNames;
     NSArray		*optionStates;
-    unsigned	selectedRow = customOptionSet;
+    unsigned	selectedTag = customOptionSet;
+    unsigned	previousSelectedTag = selectedTag;
     int			i;
     BOOL		pgp5Compatible = NO;
 
@@ -66,28 +69,48 @@ enum {
     for(i = [optionNames count] - 1; i >= 0; i--){
         NSString	*aName = [optionNames objectAtIndex:i];
 
-        if([[optionStates objectAtIndex:i] boolValue] && selectedRow == customOptionSet){
-            if([aName isEqualToString:@"rfc1991"])
-                selectedRow = rfc1991OptionSet;
-            else if([aName isEqualToString:@"pgp2"])
-                selectedRow = pgp2OptionSet;
-            else if([aName isEqualToString:@"pgp6"])
-                selectedRow = pgp6OptionSet;
-            else if([aName isEqualToString:@"openpgp"])
-                selectedRow = openPGPOptionSet;
+        if([[optionStates objectAtIndex:i] boolValue] && selectedTag == customOptionSet){
+            if([aName isEqualToString:@"rfc1991"]){
+                previousSelectedTag = selectedTag;
+                selectedTag = rfc1991OptionSet;
+            }
+            else if([aName isEqualToString:@"pgp2"]){
+                previousSelectedTag = selectedTag;
+                selectedTag = pgp2OptionSet;
+            }
+            else if([aName isEqualToString:@"no-pgp2"] && selectedTag == pgp2OptionSet)
+                selectedTag = previousSelectedTag;
+            else if([aName isEqualToString:@"pgp6"]){
+                previousSelectedTag = selectedTag;
+                selectedTag = pgp6OptionSet;
+            }
+            else if([aName isEqualToString:@"no-pgp6"] && selectedTag == pgp6OptionSet)
+                selectedTag = previousSelectedTag;
+            else if([aName isEqualToString:@"pgp7"]){
+                previousSelectedTag = selectedTag;
+                selectedTag = pgp7OptionSet;
+            }
+            else if([aName isEqualToString:@"no-pgp7"] && selectedTag == pgp7OptionSet)
+                selectedTag = previousSelectedTag;
+            else if([aName isEqualToString:@"openpgp"]){
+                previousSelectedTag = selectedTag;
+                selectedTag = openPGPOptionSet;
+            }
             else if([aName isEqualToString:@"force-v3-sigs"])
                 pgp5Compatible = YES;
+            else if([aName isEqualToString:@"no-force-v3-sigs"])
+                pgp5Compatible = NO;
         }
     }
 
-    *pgp5CompatiblePtr = (pgp5Compatible || (selectedRow == rfc1991OptionSet) || (selectedRow == pgp2OptionSet) || (selectedRow == pgp6OptionSet));
+    *pgp5CompatiblePtr = (pgp5Compatible || (selectedTag == rfc1991OptionSet) || (selectedTag == pgp2OptionSet) || (selectedTag == pgp6OptionSet) || (selectedTag == pgp7OptionSet));
 
-    return selectedRow;
+    return selectedTag;
 }
 
 - (void) updateWarning
 {
-    if([optionMatrix selectedRow] == openPGPOptionSet && [pgp5Button state]){
+    if([[optionMatrix selectedCell] tag] == openPGPOptionSet && [pgp5Button state]){
         if([warningImageView image] == nil)
             [warningImageView setImage:warningImage];
     }
@@ -99,15 +122,15 @@ enum {
 
 - (void) tabItemWillBeSelected
 {
-    unsigned	selectedRow;
+    unsigned	selectedTag;
     BOOL		pgp5Compatible;
 
     [super tabItemWillBeSelected];
-    selectedRow = [self selectedOptionSetAndPGP5Compatibility:&pgp5Compatible];
+    selectedTag = [self selectedOptionSetAndPGP5Compatibility:&pgp5Compatible];
 
-    [optionMatrix selectCellAtRow:selectedRow column:0];
-    [pgp5Button setEnabled:(selectedRow != rfc1991OptionSet && selectedRow != pgp2OptionSet && selectedRow != pgp6OptionSet)];
-    [pgp5Button setState:(pgp5Compatible || (selectedRow == rfc1991OptionSet) || (selectedRow == pgp2OptionSet) || (selectedRow == pgp6OptionSet))];
+    NSAssert1([optionMatrix selectCellWithTag:selectedTag], @"Could't find cell with tag %d!!!", selectedTag);
+    [pgp5Button setEnabled:(selectedTag != rfc1991OptionSet && selectedTag != pgp2OptionSet && selectedTag != pgp6OptionSet && selectedTag != pgp7OptionSet)];
+    [pgp5Button setState:(pgp5Compatible || (selectedTag == rfc1991OptionSet) || (selectedTag == pgp2OptionSet) || (selectedTag == pgp6OptionSet) || (selectedTag == pgp7OptionSet))];
     [self updateWarning];
 }
 
@@ -115,27 +138,44 @@ enum {
 {
     BOOL	pgp5Compatible;
 
-    switch([optionMatrix selectedRow]){
+    // Let's delete these entries
+    [[self options] setOptionValue:nil forName:@"no-pgp2"];
+    [[self options] setOptionValue:nil forName:@"no-pgp6"];
+    [[self options] setOptionValue:nil forName:@"no-pgp7"];
+
+    switch([[optionMatrix selectedCell] tag]){
         case rfc1991OptionSet:
             [[self options] setOptionState:YES forName:@"rfc1991"];
             [[self options] setOptionState:NO forName:@"pgp2"];
             [[self options] setOptionState:NO forName:@"pgp6"];
+            [[self options] setOptionState:NO forName:@"pgp7"];
             [[self options] setOptionState:NO forName:@"openpgp"];
             [pgp5Button setEnabled:NO];
             [pgp5Button setState:YES];
             break;
         case pgp2OptionSet:
+            [[self options] setOptionState:NO forName:@"rfc1991"]; // Implies rfc1991 though
             [[self options] setOptionState:YES forName:@"pgp2"];
-            [[self options] setOptionState:NO forName:@"rfc1991"];
             [[self options] setOptionState:NO forName:@"pgp6"];
+            [[self options] setOptionState:NO forName:@"pgp7"];
             [[self options] setOptionState:NO forName:@"openpgp"];
             [pgp5Button setEnabled:NO];
             [pgp5Button setState:YES];
             break;
         case pgp6OptionSet:
-            [[self options] setOptionState:YES forName:@"pgp6"];
             [[self options] setOptionState:NO forName:@"rfc1991"];
             [[self options] setOptionState:NO forName:@"pgp2"];
+            [[self options] setOptionState:YES forName:@"pgp6"];
+            [[self options] setOptionState:NO forName:@"pgp7"];
+            [[self options] setOptionState:NO forName:@"openpgp"];
+            [pgp5Button setEnabled:NO];
+            [pgp5Button setState:YES];
+            break;
+        case pgp7OptionSet:
+            [[self options] setOptionState:NO forName:@"rfc1991"];
+            [[self options] setOptionState:NO forName:@"pgp2"];
+            [[self options] setOptionState:NO forName:@"pgp6"];
+            [[self options] setOptionState:YES forName:@"pgp7"];
             [[self options] setOptionState:NO forName:@"openpgp"];
             [pgp5Button setEnabled:NO];
             [pgp5Button setState:YES];
@@ -144,6 +184,7 @@ enum {
             [[self options] setOptionState:NO forName:@"rfc1991"];
             [[self options] setOptionState:NO forName:@"pgp2"];
             [[self options] setOptionState:NO forName:@"pgp6"];
+            [[self options] setOptionState:NO forName:@"pgp7"];
             [[self options] setOptionState:YES forName:@"openpgp"];
             [pgp5Button setEnabled:YES];
             (void)[self selectedOptionSetAndPGP5Compatibility:&pgp5Compatible];
@@ -153,6 +194,7 @@ enum {
             [[self options] setOptionState:NO forName:@"rfc1991"];
             [[self options] setOptionState:NO forName:@"pgp2"];
             [[self options] setOptionState:NO forName:@"pgp6"];
+            [[self options] setOptionState:NO forName:@"pgp7"];
             [[self options] setOptionState:NO forName:@"openpgp"];
             [pgp5Button setEnabled:YES];
             (void)[self selectedOptionSetAndPGP5Compatibility:&pgp5Compatible];
@@ -166,11 +208,13 @@ enum {
 {
     BOOL	flag = [sender state];
 
-    if(flag)
+    if(flag){
         // Let's be sure that option is after openpgp option:
         // we remove it first, then add it at the end
         [[self options] setOptionValue:nil forName:@"force-v3-sigs"];
+    }
     [[self options] setOptionState:flag forName:@"force-v3-sigs"];
+    [[self options] setOptionValue:nil forName:@"no-force-v3-sigs"];
     [[self options] saveOptions];
     [self updateWarning];
 }
