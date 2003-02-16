5753987f6d3a90f064eeca4a9cc3294a2de0dbdb
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2003-02-16 21:10:53 +0000

    Changed license to LGPL (was still GPL!); now +optionsFilename depends on gpg version; added +gnupgVersion and +setGnupgVersion:

--- GPGOptions.m
+++ GPGOptions.m
@@ -5,25 +5,28 @@
 //  Created by davelopper@users.sourceforge.net on Sun Feb 03 2002.
 //
 //
-//  Copyright (C) 2002 Mac GPG Project.
+//  Copyright (C) 2002-2003 Mac GPG Project.
 //  
-//  This code is free software; you can redistribute it and/or modify it under
-//  the terms of the GNU General Public License as published by the Free
-//  Software Foundation; either version 2 of the License, or any later version.
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your options) any later version.
 //  
-//  This code is distributed in the hope that it will be useful, but WITHOUT ANY
-//  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-//  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-//  details.
-//  
-//  For a copy of the GNU General Public License, visit <http://www.gnu.org/> or
-//  write to the Free Software Foundation, Inc., 59 Temple Place--Suite 330,
-//  Boston, MA 02111-1307, USA.
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place--Suite 330, Boston, MA 02111-1307, USA
 //  
 //  More info at <http://macgpg.sourceforge.net/> or <macgpg@rbisland.cx>
 //
 
 #import "GPGOptions.h"
+
+static NSString *gnupgVersion = nil;
 
 
 @interface GPGOptions(Private)
@@ -193,7 +196,12 @@
 
 + (NSString *) optionsFilename
 {
-    return [[self homeDirectory] stringByAppendingPathComponent:@"options"];
+    NSString	*aVersion = [self gnupgVersion];
+    
+    if(aVersion == nil || [aVersion rangeOfString:@"1.0."].length > 0)
+        return [[self homeDirectory] stringByAppendingPathComponent:@"options"];
+    else
+        return [[self homeDirectory] stringByAppendingPathComponent:@"gpg.conf"];
 }
 
 - (void) parseOptionsFromLines:(NSArray *)lines save:(BOOL)save
@@ -640,6 +648,19 @@
     [self saveOptions];
 
     return [optionLineNumbers indexOfObject:[NSNumber numberWithUnsignedInt:lineIndex]];
+}
+
++ (void) setGnupgVersion:(NSString *)rawVersionString
+{
+    [rawVersionString retain];
+    [gnupgVersion release];
+    gnupgVersion = [rawVersionString copy];
+    [rawVersionString release];
+}
+
++ (NSString *) gnupgVersion
+{
+    return gnupgVersion;
 }
 
 @end
