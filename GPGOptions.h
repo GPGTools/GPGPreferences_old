bbff3dcdb6e82c891caa34ddb85bf41f5a45ec13
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2005-01-23 11:49:07 +0000

    Updated copyright; fixed use in GPGME framework

--- GPGOptions.h
+++ GPGOptions.h
@@ -2,29 +2,40 @@
 //  GPGOptions.h
 //  GPGPreferences and GPGME
 //
-//  Created by davelopper@users.sourceforge.net on Sun Feb 03 2002.
+//  Created by davelopper at users.sourceforge.net on Sun Feb 03 2002.
 //
 //
-//  Copyright (C) 2002-2003 Mac GPG Project.
+//  Copyright (C) 2002-2005 Mac GPG Project.
 //  
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your options) any later version.
+//  This code is free software; you can redistribute it and/or modify it under
+//  the terms of the GNU Lesser General Public License as published by the Free
+//  Software Foundation; either version 2.1 of the License, or (at your option)
+//  any later version.
 //  
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place--Suite 330, Boston, MA 02111-1307, USA
+//  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+//  details.
 //  
-//  More info at <http://macgpg.sourceforge.net/> or <macgpg@rbisland.cx>
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program; if not, visit <http://www.gnu.org/> or write to the
+//  Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, 
+//  MA 02111-1307, USA.
+//  
+//  More info at <http://macgpg.sourceforge.net/>
 //
 
-#import <Foundation/Foundation.h>
+#ifndef GPGOPTIONS_H
+#define GPGOPTIONS_H
+
+#include <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+extern "C" {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+#endif
 
 
 @interface GPGOptions : NSObject
@@ -119,3 +130,8 @@
 // Returns all values for option name whose state is active
 
 @end
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* GPGOPTIONS_H */
